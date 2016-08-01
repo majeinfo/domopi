02d52715f6fc2aeec53b0307507f9965317ddd91
JD <jd@maje.biz>
2016-08-01 14:42:52 +0200

    change dbserver name

--- domopi/settings.py
+++ domopi/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
+        'HOST': 'dbserver',
         'USER': 'domopi',
         'PASSWORD': 'domopi',
         'NAME': 'domopi',
